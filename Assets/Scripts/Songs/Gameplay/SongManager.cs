e21e3d69fc731cf4795b0b8791c2aa7cb7a5f014
Matthew G <matthewgrant55@gmail.com>
2019-09-30 19:58:08 -0400

    Finished scoring system

--- Assets/Scripts/Songs/Gameplay/SongManager.cs
+++ Assets/Scripts/Songs/Gameplay/SongManager.cs
@@ -46,29 +46,69 @@ namespace Songs.Gameplay {
 				}
 			}
 		}
-
+		public int score = 0;
+		public int numCorrect = 0;
+		PressAccuracy accuracy = PressAccuracy.Miss;
 		private void senseKeyPresses() {
 			//sense which keys are pressed
 			foreach (int laneIdx in input.keysIndiciesPressedButton) {
 				Lane lane = lanes[laneIdx];
 				lane.makePressVFx();
-
 				GameObject lowestNote = lane.getLowestNote();
+
+				int scoreMult = Multiplier(numCorrect);
+
 				if (lowestNote != null) {
 					float distance = (lowestNote.transform.position - lane.noteTarget.transform.position).magnitude;
 					if (distance < 0.5) {
 						lane.noteTapVFx(PressAccuracy.Perfect);
+						score += 100*scoreMult;
+						numCorrect += 1;
 					} else if (distance < 1) {
 						lane.noteTapVFx(PressAccuracy.Good);
+						score += 75*scoreMult;
+						numCorrect += 1;
 					} else {
 						lane.noteTapVFx(PressAccuracy.Miss);
+						scoreMult = 0;
+						numCorrect = 0;
 					}
 				}
+
 				Destroy(lowestNote);
 			}
 			//if the answer is close. for now. just be happy.
 			//if the answer is far. for now. just be sad.
 			//maybe one for medium distance if you can swing it.
 		}
+		public static int Multiplier(int numCorrect)
+		{
+			if (numCorrect > 200)
+			{
+				return 8;
+			}
+			else if (numCorrect > 100)
+			{
+				return 4;
+			}
+			else if (numCorrect > 50)
+			{
+				return 2;
+			}
+			else
+			{
+				return 1;
+			}
+
+		}
+
+
+
+
+
+
+
+		
+
 	}
 }
