6ffbe306aa1d2a4675691532e54b2b69489ef394
Cinnamon18 <alice.ava.moh@gmail.com>
2019-11-01 14:01:50 -0400

    backgrounds!

--- Assets/Scripts/Cutscenes/CutsceneVisualsManager.cs
+++ Assets/Scripts/Cutscenes/CutsceneVisualsManager.cs
@@ -3,10 +3,12 @@ using System.Collections;
 using Cutscene.Elements;
 using UnityEngine;
 using UnityEngine.UI;
+using Utilities;
 
 namespace Cutscene {
 	public class CutsceneVisualsManager : MonoBehaviour {
 		public GameObject canvas;
+		public Image background;
 
 		public IEnumerator addActor(Actor actor, CutscenePosition position) {
 			yield return null;
@@ -16,5 +18,16 @@ namespace Cutscene {
 		public IEnumerator removeActor(Actor actor) {
 			yield return null;
 		}
+
+		public IEnumerator setBackground(Sprite newBackgrond, float time) {
+			if (time == 0) {
+				this.background.sprite = newBackgrond;
+			} else {
+				yield return Util.fadeOut(background, time);
+				this.background.sprite = newBackgrond;
+				yield return Util.fadeIn(background, time);
+			}
+		}
+
 	}
 }
