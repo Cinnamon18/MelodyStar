713feed4a5051c0e843d2355f9b06a04cbe203f3
L. Bowers <31969905+Bowers-L@users.noreply.github.com>
2019-11-11 21:38:48 -0500

    Added Player Preferences

--- Assets/Scripts/Menuing/MainMenu.cs
+++ Assets/Scripts/Menuing/MainMenu.cs
@@ -6,23 +6,42 @@ namespace Menuing {
 	public class MainMenu : MonoBehaviour {
 
 		void Start() {
-			InputSettings.setToDefault();
+			if(PlayerPrefs.HasKey("saved"))
+			{
+				InputSettings.setToPrefs();
+			} else if (!InputSettings.initalized) {
+				InputSettings.setToDefault();
+			}
 		}
 
 		public void story() {
+			saveSettings();
 			StartCoroutine(SceneTransition.LoadScene("Story"));
 		}
 
 		public void freePlay() {
+			saveSettings();
 			StartCoroutine(SceneTransition.LoadScene("Song"));
 		}
 
 		public void configure() {
+			saveSettings();
 			StartCoroutine(SceneTransition.LoadScene("Configure"));
 		}
 
 		public void quit() {
+			saveSettings();
 			Application.Quit();
+		}
+
+		private void saveSettings()
+		{
+			//TESTING
+			//Debug.Log("Saved Settings");
+			PlayerPrefs.SetInt("saved", 1);
+			PlayerPrefs.SetString("keys", InputSettings.getKeysString());
+			PlayerPrefs.SetInt("inputMode", (int) InputSettings.inputMode);
+			PlayerPrefs.SetInt("middleC", InputSettings.middleC);
 		}
 
 	}
