babdc5fe18b83c9bde5a381ccb09c3cf71c63242
L. Bowers <31969905+Bowers-L@users.noreply.github.com>
2019-11-11 21:55:41 -0500

    Saving moved to config screen

--- Assets/Scripts/Menuing/MainMenu.cs
+++ Assets/Scripts/Menuing/MainMenu.cs
@@ -15,34 +15,22 @@ namespace Menuing {
 		}
 
 		public void story() {
-			saveSettings();
 			StartCoroutine(SceneTransition.LoadScene("Story"));
 		}
 
 		public void freePlay() {
-			saveSettings();
 			StartCoroutine(SceneTransition.LoadScene("Song"));
 		}
 
 		public void configure() {
-			saveSettings();
 			StartCoroutine(SceneTransition.LoadScene("Configure"));
 		}
 
 		public void quit() {
-			saveSettings();
 			Application.Quit();
 		}
 
-		private void saveSettings()
-		{
-			//TESTING
-			//Debug.Log("Saved Settings");
-			PlayerPrefs.SetInt("saved", 1);
-			PlayerPrefs.SetString("keys", InputSettings.getKeysString());
-			PlayerPrefs.SetInt("inputMode", (int) InputSettings.inputMode);
-			PlayerPrefs.SetInt("middleC", InputSettings.middleC);
-		}
+
 
 	}
 }
